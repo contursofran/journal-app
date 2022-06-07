10039e8c77ab75e5e01ce40e71029fefedc7c71d
fconturso <contursofran@gmail.com>
2022-06-06 23:15:49 -0300

    Initial commit

---
+++ client/src/index.tsx
@@ -0,0 +1,16 @@
+import { MantineProvider } from "@mantine/core";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+root.render(
+  <MantineProvider
+    theme={{ colorScheme: "dark" }}
+    withGlobalStyles
+    withNormalizeCSS
+  >
+    <App />
+  </MantineProvider>
+);
