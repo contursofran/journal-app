533a0d3663ca63236c9fc96a0b636ff732d0bfaf
fconturso <contursofran@gmail.com>
2022-06-12 18:55:38 -0300

    Implement devtools for zustand

--- client/src/store/store.ts
+++ client/src/store/store.ts
@@ -1,9 +1,12 @@
 import create from "zustand";
+import { devtools } from "zustand/middleware";
 import createCalendarSlice from "./createCalendarSlice";
 import { StoreState } from "../types";
 
-const useStore = create<StoreState>((set, get) => ({
-  ...createCalendarSlice(set, get),
-}));
+const useStore = create<StoreState>()(
+  devtools((set, get) => ({
+    ...createCalendarSlice(set, get),
+  }))
+);
 
 export default useStore;
