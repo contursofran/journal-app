887e940079dc1bdbe329ad0380a690bbb0d0a729
fconturso <contursofran@gmail.com>
2023-10-25 19:43:22 -0300

    fix: api url

--- client/src/utils/constants.ts
+++ client/src/utils/constants.ts
@@ -1,6 +1,6 @@
 export const apiUrl =
   process.env.ENV === "development"
     ? "http://localhost:3001"
-    : "https://journal-app-ki9h.onrender.com/";
+    : "https://journal-app-ki9h.onrender.com";
 
 export const colors = ["red", "blue", "cyan", "yellow", "violet"];
