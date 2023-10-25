b0287e93863d6d260e55d48b60af36cbfb5ec00e
fconturso <contursofran@gmail.com>
2023-10-25 12:11:05 -0300

    feat: move api_url to .env

--- client/src/utils/constants.ts
+++ client/src/utils/constants.ts
@@ -1,3 +1,6 @@
-export const apiUrl = "https://journal-app-production-ccff.up.railway.app";
+export const apiUrl =
+  process.env.ENV === "development"
+    ? "http://localhost:3001"
+    : process.env.API_URL;
 
 export const colors = ["red", "blue", "cyan", "yellow", "violet"];
