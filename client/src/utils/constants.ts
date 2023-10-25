4343876e0642a73c3fc1cb5dbd2ad5e995f71a17
fconturso <contursofran@gmail.com>
2023-10-25 19:39:16 -0300

    feat: move server deploy to render

--- client/src/utils/constants.ts
+++ client/src/utils/constants.ts
@@ -1,6 +1,6 @@
 export const apiUrl =
   process.env.ENV === "development"
     ? "http://localhost:3001"
-    : process.env.API_URL;
+    : "https://journal-app-ki9h.onrender.com/";
 
 export const colors = ["red", "blue", "cyan", "yellow", "violet"];
