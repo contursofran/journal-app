56a11d034c86effacbb29357cc6340858ad0008a
fconturso <contursofran@gmail.com>
2023-10-25 00:07:40 -0300

    chore: move firebase google credentials to env variable

--- server/firebase.ts
+++ server/firebase.ts
@@ -1,7 +1,9 @@
 import admin from "firebase-admin";
 
+const serviceAccount = JSON.parse(process.env.GOOGLE_CREDENTIALS || "");
+
 admin.initializeApp({
-  credential: admin.credential.applicationDefault(),
+  credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://<DATABASE_NAME>.firebaseio.com",
 });
 
